6facf66d5da3fd5e4602c657a98addcccc84a33a
rainu <rainu@raysha.de>
2018-06-13 20:41:15 +0200

    implements rest-endpoint for course-aggregator

---
+++ aggregator/course/src/server.js
@@ -0,0 +1,25 @@
+const coursefinder = require('./coursefinder');
+const express = require('express')
+
+var app = express()
+
+app.route('/api/ratios/:type/:symbol')
+  .get(function(req, res) {
+    coursefinder({
+      name: req.params.name,
+      type: req.params.type
+    })
+    .then(ratios => {
+      res.send({
+        ratios: ratios
+      })
+    })
+    .catch(err => {
+      res.status(500);
+      res.send({
+        error: err
+      })
+    })
+  })
+
+app.listen(3000)
