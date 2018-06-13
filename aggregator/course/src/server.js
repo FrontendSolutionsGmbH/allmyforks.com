4a8381a89f4479fcb28d24fef09471eb545faca5
rainu <rainu@raysha.de>
2018-06-13 20:44:52 +0200

    add endpoint for get pairs

--- aggregator/course/src/server.js
+++ aggregator/course/src/server.js
@@ -1,4 +1,5 @@
 const coursefinder = require('./coursefinder');
+const pairfinder = require('./pairfinder')
 const express = require('express')
 
 var app = express()
@@ -12,6 +13,22 @@ app.route('/api/ratios/:type/:symbol')
     .then(ratios => {
       res.send({
         ratios: ratios
+      })
+    })
+    .catch(err => {
+      res.status(500);
+      res.send({
+        error: err
+      })
+    })
+  })
+
+app.route('/api/pairs')
+  .get(function(req, res) {
+    pairfinder()
+    .then(pairs => {
+      res.send({
+        pairs: pairs
       })
     })
     .catch(err => {
