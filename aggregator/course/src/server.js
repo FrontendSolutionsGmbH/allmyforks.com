86b0115025bdcf59f12fdf1589a99b052a25db4b
rainu <rainu@raysha.de>
2018-07-05 16:58:14 +0200

    add endpoint for making heap-dump

--- aggregator/course/src/server.js
+++ aggregator/course/src/server.js
@@ -1,8 +1,29 @@
 const coursefinder = require('./coursefinder');
 const pairfinder = require('./pairfinder')
 const express = require('express')
+const config = require('./config');
+const heapdump = require('heapdump')
 
 var app = express()
+
+app.route('/api/__debug/heapdump')
+  .get(function(req, res) {
+    const fileName = `${config.debug.heap.dump.directory}/heapdump-${Date.now()}.heapsnapshot`
+
+    heapdump.writeSnapshot(fileName, function(err, filename) {
+      if(err) {
+        res.status(500)
+        res.send({
+          error: err
+        })
+      } else {
+        res.status(200)
+        res.send({
+          file: filename
+        })
+      }
+    });
+  })
 
 app.route('/api/ratios/:type/:symbol')
   .get(function(req, res) {
